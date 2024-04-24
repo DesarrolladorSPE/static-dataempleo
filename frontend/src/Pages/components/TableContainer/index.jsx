93de7de677995ce6544e7bed7102827c11510a8c
5arSanti <santiari05@hotmail.com>
2024-04-23 20:11:46 -0500

    Change in Container

--- frontend/src/Pages/components/TableContainer/index.jsx
+++ frontend/src/Pages/components/TableContainer/index.jsx
@@ -1,15 +1,15 @@
 import { Title } from "../Title";
-import { WrapperContainer1 } from "../WrapperContainers";
+import { WrapperContainer2 } from "../WrapperContainers";
 import { Table } from "./Table";
 
 const TableContainer = () => {
     return(
-        <WrapperContainer1 flexDirection="column" gap={5} padding={25}>
+        <WrapperContainer2 flexDirection="column" gap={5} padding={25}>
             <Title color="#000">
                 {`Tabla`}
             </Title>
             <Table/>
-        </WrapperContainer1>
+        </WrapperContainer2>
     );
 }
 
