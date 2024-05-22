5c58d3cfe6afbe8c38d9344c2480235cb9c11a46
5arSanti <santiari05@hotmail.com>
2024-05-21 19:56:42 -0500

    New files and changes

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -12,7 +12,8 @@ import a_anexoBuscadoresDeEmpleoMarzo from "./Files/A. Anexo buscadores de emple
 import a_boletinOfertaLaboralMarzo2024 from "./Files/A. Boletin Oferta Laboral Marzo 2024.pdf"
 
 import b_anexoOfertasDeEmpleoMarzo from "./Files/B. Anexo ofertas de empleo (Marzo).xlsx"
-import b_boletinDemandaLaboralMarzo2024 from "./Files/B. Boletin Demanda Laboral Marzo 2024.pdf"
+import b_anexoOfertasDeEmpleoSISEMarzo from "./Files/B. Anexo ofertas de empleo SISE (Marzo).xlsx"
+import b_boletinDemandaLaboralRedSISEMarzo2024 from "./Files/B. Boletín Demanda Laboral (RED + SISE) Marzo 2024.pdf"
 
 import c_anexoColocacionesMarzo from "./Files/C. Anexo colocaciones (Marzo).xlsx"
 import c_boletinColocacionesMarzo2024 from "./Files/C. Boletin Colocaciones Marzo 2024.pdf"
@@ -31,7 +32,8 @@ export {
     a_anexoBuscadoresDeEmpleoMarzo,
     a_boletinOfertaLaboralMarzo2024,
     b_anexoOfertasDeEmpleoMarzo,
-    b_boletinDemandaLaboralMarzo2024,
+    b_boletinDemandaLaboralRedSISEMarzo2024,
+    b_anexoOfertasDeEmpleoSISEMarzo,
     c_anexoColocacionesMarzo,
     c_boletinColocacionesMarzo2024,
 }
