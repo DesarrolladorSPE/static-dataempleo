732cc142de6bb724326942c24fae1be5492d8b45
5arSanti <santiari05@hotmail.com>
2024-06-24 16:20:42 -0500

    Deleted unnecesary assets, new icon

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,9 +1,7 @@
 import banner from "./banner.png";
-import datosLista from "./datos-lista.png";
 import logoCoFooter from "./logo_co_footer.png";
 import logoFooter from "./logo_footer.png";
 import iconComplete from "./icon-complete.png"
-import logoCoPotencia from "./log_co_potencia.png"
 
 import Montserrat from "./fonts/Montserrat/Montserrat-Regular.ttf";
 import MontserratBold from "./fonts/Montserrat/Montserrat-Bold.ttf";
@@ -23,11 +21,9 @@ import correlativaSISECuocUnidadSPE from "./Files/CORRELATIVA SISE-CUOC TERMINAD
 
 export {
     banner,
-    datosLista,
     logoCoFooter,
     logoFooter,
     iconComplete,
-    logoCoPotencia,
     Montserrat,
     MontserratBold,
 
